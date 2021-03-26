b37cc01b960d6446dcf9bc1028b245959e62f1ae
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2021-03-26 23:47:49 +0600

    BFS DFS implementation

---
+++ BFS DFS implementation.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+int mod(string n, ll a)
+{
+
+    ll res = 0;
+    for (int i = 0; i < n.length(); i++)
+         res = (res*10 + (n[i]-'0')) % a;
+
+    return res;
+}
+vector<int>e[MAX];
+bool visited[MAX];
+void DFS(int node)
+{
+    visited[node]=true;
+    cout<<node<<' ';
+    for(auto x: e[node])
+    {
+        if(!visited[x])
+        {
+            DFS(x);
+        }
+    }
+}
+void BFS()
+{
+    queue<int>s;
+    s.push(1);
+    visited[1]=true;
+    while(!s.empty())
+    {
+        cout<<s.front()<<' ';
+        for(auto x:e[s.front()])
+        {
+            if(!visited[x])
+            {
+                s.push(x);
+                visited[x]=1;
+            }
+        }
+        s.pop();
+    }
+
+}
+int main()
+{
+    int n,p;
+    cin>>n>>p;
+    while(n--)
+    {
+        int u,v;
+        cin>>u>>v;
+        e[u].emplace_back(v);
+    }
+/*
+    for(int i=1;i<=p;i++)
+    {
+        if(!visited[i])
+        {
+            DFS(i);
+        }
+    }
+    cout<<endl;
+    */
+    BFS();
+
+}
